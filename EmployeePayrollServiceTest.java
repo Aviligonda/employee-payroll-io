39439d877119b1ea43b6353a8f6deacef7e1a735
aviligonda <aviligondasreenivasulu6086@gmail.com>
2022-07-13 13:30:38 +0530

    Added : Employee Payroll data store into a file

---
+++ EmployeePayrollServiceTest.java
@@ -0,0 +1,23 @@
+package com.bridzelab;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+public class EmployeePayrollServiceTest {
+    @Test
+    public void given3EmployeeWhenWrittenToFileShouldMatchEmployeeEntries() {
+        EmployeePayrollData[] arrayOfEmp = {
+                new EmployeePayrollData(1, "SR", 10000),
+                new EmployeePayrollData(2, "MB", 20000),
+                new EmployeePayrollData(3, "AK", 30000)
+        };
+        EmployeePayrollService employeePayRollService;          //uc3 save the details in a file --open terminal enter->> more payroll-file.txt
+        employeePayRollService = new EmployeePayrollService(Arrays.asList(arrayOfEmp));
+        employeePayRollService.writeEmployeePayrollDara(EmployeePayrollService.IOService.FILE_IO);
+        long entries = employeePayRollService.countEntries(EmployeePayrollService.IOService.FILE_IO);
+        Assert.assertEquals(3, entries);
+    }
+
+}
